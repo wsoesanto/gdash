ba370fd1cc2db5a89a930cf291e5c9ee31b2622b
Willy <opelhoward@yahoo.com>
2016-05-25 12:27:39 +0700

    Activate the menu javascript, click to toggle it inside and outside

--- gdash/static/js/views/layout.js
+++ gdash/static/js/views/layout.js
@@ -1,18 +1,26 @@
 "use strict";
 
 define([
-    "text!templates/volume/layout.html",
-    "views/volume/list"
-], function (layoutTemplate, ListView) {
-    return Backbone.View.extend({
-        el: $("#main-container"),
-        initialize: function (router) {
-            this.router = router;
-        },
-        render: function () {
-            this.$el.append(_.template(layoutTemplate));
-            this.listView = new ListView(this.router);
-            this.listView.render();
-        }
-    });
+  "text!templates/volume/layout.html",
+  "views/volume/list"
+], function(layoutTemplate, ListView) {
+  return Backbone.View.extend({
+    el: $("#main-container"),
+    initialize: function(router) {
+      this.router = router;
+    },
+    render: function() {
+      this.$el.append(_.template(layoutTemplate));
+      this.listView = new ListView(this.router);
+      this.listView.render();
+
+      $("#menuLink").click(this.showMenu);
+    },
+    showMenu: function() {
+      var activeClass = "active";
+      $("#layout").toggleClass(activeClass);
+      $("#menu").toggleClass(activeClass);
+      $("#menuLink").toggleClass(activeClass);
+    }
+  });
 });
