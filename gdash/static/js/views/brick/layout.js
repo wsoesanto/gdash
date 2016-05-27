98fa4d4278caa0edfc2608b53550633bffd6e788
Willy <opelhoward@yahoo.com>
2016-05-27 08:57:28 +0700

    Able to display clients brick layout from the given layout text

--- gdash/static/js/views/brick/layout.js
+++ gdash/static/js/views/brick/layout.js
@@ -9,6 +9,7 @@ define([
     dirTreeTemplate: _.template(dirTreeTemplate),
     initialize: function(volumeName) {
       this.volumeName = volumeName;
+      $("#client-button").click(this.readClientLayout(this));
     },
     render: function() {
       this.$el.html("Unable to get file tree information");
@@ -20,7 +21,7 @@ define([
       }).done(function(data) {
         var $content = _this.renderDirTree("root", data, _this);
         _this.$el.html($content);
-        _this.$el.delegate("li a", "click", _this.renderGraph);
+        _this.$el.delegate("li a", "click", _this.renderGraph($("#brick-chart")));
       });
     },
     renderDirTree: function(name, dirLayout, _this) {
@@ -36,56 +37,98 @@ define([
       $content.find(".directory").append($subfoldersContent);
       return $content;
     },
-    renderGraph: function(event) {
-      var $el = $(event.target);
-      var $layouts = $el.next();
+    renderGraph: function($chart) {
+      return function(event) {
+        var $el = $(event.target);
+        var $layouts = $el.next();
 
-      var xAxisData = [];
-      var yAxisData = [];
-      $layouts.children().each(function() {
-        xAxisData.push(
-          $(this).attr("host")
-        );
-        yAxisData.push({
-          low: Number($(this).attr("start")),
-          high: Number($(this).attr("end"))
+        var xAxisData = [];
+        var yAxisData = [];
+        $layouts.children().each(function() {
+          xAxisData.push(
+            $(this).attr("host")
+          );
+          yAxisData.push({
+            low: Number($(this).attr("start")),
+            high: Number($(this).attr("end"))
+          });
         });
-      });
 
-      $("#brick-chart").highcharts({
-        exporting: {
-          enabled: false
-        },
-        title: {
-          text: "Volume's layout"
-        },
-        chart: {
-          type: 'columnrange',
-          inverted: true,
-          animation: false
-        },
-        plotOptions: {
-          columnrange: {
-            dataLabels: {
-              enabled: true,
-              formatter: function() {
-                return "0x" + this.y.toString(16);
-              }
-            },
+        $chart.highcharts({
+          exporting: {
+            enabled: false
+          },
+          title: {
+            text: "Volume's layout"
+          },
+          chart: {
+            type: 'columnrange',
+            inverted: true,
             animation: false
+          },
+          plotOptions: {
+            columnrange: {
+              dataLabels: {
+                enabled: true,
+                formatter: function() {
+                  return "0x" + this.y.toString(16);
+                }
+              },
+              animation: false
+            }
+          },
+          series: [{
+            name: 'Range',
+            data: yAxisData
+          }],
+          xAxis: {
+            categories: xAxisData
+          },
+          yAxis: {
+            visible: false
           }
-        },
-        series: [{
-          name: 'Range',
-          data: yAxisData
-        }],
-        xAxis: {
-          categories: xAxisData
-        },
-        yAxis: {
-          visible: false
-        }
-      });
+        });
+      };
+    },
+    readClientLayout: function(_this) {
+      return function() {
+        var layoutData = {
+          layouts: {},
+          subfolders: {}
+        };
+        var layoutText = $("#client-layout").val();
+        _.each(layoutText.split('\n'), function(dirLayoutsText) {
+          if (dirLayoutsText === "") {
+            return;
+          }
+          _.each(dirLayoutsText.split(' '), function(dirLayoutText) {
+            var dirLayoutArr = dirLayoutText.split(',');
+            var dirsArr = dirLayoutArr[0].split('/');
+            var dirLayout = layoutData;
+            _.each(dirsArr, function(dirname) {
+              if (dirname === "") {
+                return;
+              }
+              if (_.isUndefined(dirLayout.subfolders[dirname])) {
+                dirLayout.subfolders[dirname] = {
+                  layouts: {},
+                  subfolders: {}
+                };
+              }
+              dirLayout = dirLayout.subfolders[dirname];
+            });
+            var layouts = dirLayout.layouts;
+            layouts[dirLayoutArr[4] + ":" + dirLayoutArr[5]] = {
+              start: Number(dirLayoutArr[2]),
+              end: Number(dirLayoutArr[3])
+            };
+          });
+        });
+        var $content = _this.renderDirTree("root", layoutData, _this);
+        var $clientTree = $("#client-directory-tree");
+        $clientTree.html($content);
+        $clientTree.delegate("li a", "click", _this.renderGraph($("#client-brick-chart")));
+      };
     }
   });
 });
